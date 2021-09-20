23a1e1df72ba4007353b2960622a067befd27ed0
Паша Смирнов <90782743+Nigmak@users.noreply.github.com>
2021-09-21 01:17:48 +0300

    Add files via upload

--- client.py
+++ client.py
@@ -1,12 +1,12 @@
 import socket
-
+from time import sleep
 
 sock = socket.socket()
 sock.setblocking(1)
 sock.connect(('localhost', 9090))
 
 msg =input()
-msg="Hi"
+msg="End"
 
 sock.send(msg.encode())
 
--- server.py
+++ server.py
@@ -19,3 +19,4 @@ print(msg)
 
 if msg == 'exit':
         conn.close()
+
